5968e7dd4eab34f36792b522f001a2a98039709c
hujia from MacBook <jhu@pasonatquila.com>
2014-03-25 14:50:27 +0900

    1

--- src/main/java/com/example/HelloServlet.java
+++ src/main/java/com/example/HelloServlet.java
@@ -17,7 +17,7 @@ public class HelloServlet extends HttpServlet {
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
         
-        out.write("Hello Heroku".getBytes());
+        out.write("Hello Heroku by Jia Hu !".getBytes());
         out.flush();
         out.close();
     }
